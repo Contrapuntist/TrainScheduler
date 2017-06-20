9a05114bce5a70bd15ed13e900ff7cd88040899a
Contrapuntist <contrapuntist@gmail.com>
2017-06-19 19:20:00 -0500

    got row remove to work

--- javascript/trainscheduler.js
+++ javascript/trainscheduler.js
@@ -7,12 +7,17 @@ $(document).ready(function() {
   firebase.initializeApp(config);
   var database = firebase.database(); 
 
+
+  var rootRef = firebase.database().ref();
+  var key = rootRef.key;  // key === null
+  console.log(rootRef);
+  console.log(key);
+  
   // SECTION FOR TESTING MOMENT.JS 
   function testmoment () { 
     var timetest = moment();
     console.log (timetest) 
   } 
-
   testmoment();  
 
 
@@ -89,8 +94,8 @@ $(document).ready(function() {
       destination: trSched.destination,
       firstTrainTime: trSched.firstTrain,
       trainFrequency: trSched.frequency,
-    });
- 
+    }); 
+
   }); 
 
   database.ref().on("child_added", function(childSnapshot) { 
@@ -100,66 +105,20 @@ $(document).ready(function() {
       var trDestin = childSnapshot.val().destination;
       var trTime = childSnapshot.val().firstTrainTime;
       var trFreq = childSnapshot.val().trainFrequency;
-      
+      var datakey = childSnapshot.key;
+      console.log(datakey);
       // placeholder field for now
 
-      
       console.log(trTime);
       console.log(trFreq);
 
-      // // Generatates new row
-      // var newRow = $("<tr>"); 
-      // newRow.html("<td>" + trName + "</td><td>" + trDestin + "</td><td>" + trFreq + "</td><td>" + trTime + "</td><td>" + nxtTr + "</td>");
-      
-      // // adds row to table
-      // $('#trains').append(newRow);     
-    
 
-    // function trainTimeCalc () {
-      
-    // var tFrequency = trFreq;
-
-    // // Time is 3:30 AM
-    // var firstTime = trTime;
-
-    // // First Time (pushed back 1 year to make sure it comes before current time)
-    // var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
-
-    // // Current Time
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // // Difference between the times
-    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // // Time apart (remainder)
-    // var tRemainder = diffTime % tFrequency;
-    // console.log(tRemainder);
-
-    // // Minute Until Train
-    // var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // // Next Train
-    // nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
-
-    // nextTrain = moment(nextTrain).format("HH:mm"); 
-    // console.log(nextTrain); 
-
-    // minAway = tMinutesTillTrain;
-    // console.log(minAway);
-
-    // } 
-    
     trSched.trainTimeCalc(trTime, trFreq); 
 
     var newRow = $("<tr>"); 
     newRow.html("<td>" + trName + "</td><td>" + trDestin + "</td><td>" 
       + trFreq + "</td><td>" + trSched.nextTrain + "</td><td>" + trSched.minAway + 
-      "</td><td><button class='clearbox'>X</button></td>");
+      "</td><td><button class='clearbtn' id='" + datakey + "'>X</button></td>");
     
     // adds row to table
     $('#trains').append(newRow);  
@@ -169,10 +128,19 @@ $(document).ready(function() {
   });
 
 
-  $("#trains").on('click', '.clearbbox', function () {
+  $("table").on('click', '.clearbtn', function () {
+    
+    var dataNode = $(this).attr("id");
+    
+    database.ref().child(dataNode).remove();
     $(this).closest('tr').remove();
-    console.log('delete button clicked');
+    
+    // console.log('delete button clicked');
+    // console.log(dataNode);
+    // location.reload();
+    
 });
+ 
 
 
 });
